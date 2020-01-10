728e641890d5d81da23edb1e1cbb8657a1f771e0
janelle mellor <janellemellor@gmail.com>
2020-01-10 13:29:52 -0800

    add event listener

---
+++ app.js
@@ -0,0 +1,37 @@
+function fillInBlank() {
+
+    const nounInputOne = document.getElementById('noun input one');
+    const nounOutputOne = nounInputOne.value;
+    nounOne.textcontent = nounOutputOne;
+
+    const nounInputTwo = document.getElementById('noun input two');
+    const nounOutputTwo = nounInputTwo.value;
+    nounTwo.textContent = nounOutputTwo;
+
+    const verbInputOne = document.getElementById('verb input one');
+    const verbOutputOne = verbInputOne.value;
+    verbOne.textContent = verbOutputOne;
+
+    const nounInputThree = document.getElementById('noun input three');
+    const nounOutputThree = nounInputThree.value
+    nounThree.textContent = nounOutputThree;
+
+    const verbInputTwo = document.getElementById('verb input two');
+    const verbOutputTwo = verbInputTwo.value;
+    verbTwo.textContent = verbOutputTwo;
+
+    const nounInputFour = document.getElementById('noun input four');
+    const nounOutputFour = nounInputFour.value;
+    nounFour.textContent = nounOutputFour;
+
+    const nounInputFive = document.getElementById('noun input five');
+    const nounOutputFive = nounInputFive.value;
+    nounFive.textContent = nounOutputFive;
+
+    const nounInputSix = document.getElementById('noun input six');
+    const nounOutputSix = nounInputSix.value;
+    nounSix.textContent = nounOutputSix;
+
+}
+
+button.addEventListener('click', fillInBlank);
